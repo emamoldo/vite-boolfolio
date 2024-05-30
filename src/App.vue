f0823dea388df3070054b596431ad26a4d912460
emamoldo <moldovanemanuel92.em@gmail.com>
2024-05-30 17:34:35 +0200

    Add Milestone 3 and Milestone 4

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      base_api_url: 'http://127.0.0.1:8000',
+      base_projects_url: '/api/projects',
+      projects: [],
+      loading: true
+    }
+  },
+
+  methods: {
+
+    callAPI(url) {
+      axios
+        .get(url)
+        .then(response => {
+          this.projects = response.data.projects
+          // console.log(response.data.projects);
+          this.loading = false
+        })
+        .catch(err => {
+          console.error(err);
+        })
+    }
+  },
+
+  mounted() {
+    let url = this.base_api_url + this.base_projects_url
+    this.callAPI(url);
+    console.log();
+  }
+}
+</script>
+
+<template>
+  <header>
+    Header Here
+  </header>
+
+  <main>
+    Main Here
+  </main>
+
+  <footer class="py-5 bg-dark text-white">
+    Footer Here
+  </footer>
+</template>
+
+<style></style>
