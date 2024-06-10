765e406d2dee9c909d298f7672a2f0d091a53d00
emamoldo <moldovanemanuel92.em@gmail.com>
2024-06-10 17:41:14 +0200

    Add the pagination for the projects

--- src/views/AppBlog.vue
+++ src/views/AppBlog.vue
@@ -16,30 +16,28 @@ export default {
 
     methods: {
 
-        /*
-            goTo(page) {
-                const url = this.base_api_url + this.base_projects_url + `?page=${page}`
-                console.log(url);
-                this.callAPI(url)
-            },
+        goTo(page) {
+            const url = this.base_api_url + this.base_projects_url + `?page=${page}`
+            console.log(url);
+            this.callAPI(url)
+        },
 
-            prevPage(url) {
-                this.callAPI(url)
-                console.log(url);
-            },
+        prevPage(url) {
+            this.callAPI(url)
+            console.log(url);
+        },
 
-            nextPage(url) {
-                this.callAPI(url)
-                console.log(url);
-            },
-        */
+        nextPage(url) {
+            this.callAPI(url)
+            console.log(url);
+        },
 
         callAPI(url) {
             axios
                 .get(url)
                 .then(response => {
                     this.projects = response.data.projects
-                    // console.log(response.data.projects);
+                    console.log(this.projects);
                 })
                 .catch(err => {
                     console.error(err);
@@ -50,20 +48,19 @@ export default {
     mounted() {
         let url = this.base_api_url + this.base_projects_url
         this.callAPI(url);
-        console.log();
     }
 }
 </script>
 
 <template>
 
-    <AppBanner class="py-5" title="BoolFolio Projects" leadText="Look for our Projects" callToAction="More About Us"
+    <AppBanner class="py-3" title="BoolFolio Projects" leadText="Look for our Projects" callToAction="More About Us"
         callToActionUrl="about">
     </AppBanner>
 
     <div class="container text-center py-5">
         <div class="row row-cols-2 row-cols-lg-6 d-flex gap-3">
-            <div class="cards card py-2" v-for="project in projects">
+            <div class="cards card py-2" v-for="project in projects.data">
 
                 <router-link :to="{ name: 'singleProject', params: { id: project.id } }">
                     <template v-if="project.cover_image.startsWith('uploads')">
@@ -81,6 +78,19 @@ export default {
                 </div>
             </div>
         </div>
+
+        <nav aria-label="Page navigation">
+            <ul class="pagination py-3">
+                <li class="page-item" v-show="projects.previous_page_url" @click="prevPage(projects.prev_page_url)">
+                </li>
+                <li class="page-item" :class="{ 'active': page == projects.current_page }"
+                    v-for="page in projects.last_page" @click="goTo(page)">
+                    <buttoon class="page-link">{{ page }}</buttoon>
+                </li>
+                <li class="page-item" v-show="projects.next_page_url" @click="nextPage(projects.next_page_url)"></li>
+            </ul>
+        </nav>
+
     </div>
 
 </template>
